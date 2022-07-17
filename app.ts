677f7b62b5dfcc34719e56200874093109e74210
Ashley Travaini <ashleytravaini@hotmail.com>
2022-07-17 10:45:21 +1000

    Reordering of project layout

--- app.ts
+++ app.ts
@@ -1,8 +1,7 @@
-import Game from "./game"
+import Game from "./Gomoku/game"
 
 // Gomoku game is created and run here. 
 let main = document.getElementById('main');
 if (main !== null) {
     const game = new Game(15, 15, main, 2);
 }
-
